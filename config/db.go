cadbe83e004c876f821c1aaf8465e5436d703457
Bhuwan Darai <dr.darai29@gmail.com>
2025-06-06 12:19:59 +0545

    updated database config

--- config/db.go
+++ config/db.go
@@ -3,15 +3,16 @@ package config
 import (
 	"log"
 
-	prisma "github.com/bhuwan-darai/crud/prisma/db"
+	// prisma "github.com/bhuwan-darai/crud/prisma/db"
+	"github.com/bhuwan-darai/crud/prisma/db"
 )
 
 // DB holds the Prisma client
 
-func ConnectDB() *prisma.PrismaClient {
-	db := prisma.NewClient()
-	if err := db.Prisma.Connect(); err != nil {
+func ConnectDB() *db.PrismaClient {
+	database := db.NewClient()
+	if err := database.Prisma.Connect(); err != nil {
 		log.Fatalf("Error connecting database :%v", err)
 	}
-	return db
+	return database
 }
